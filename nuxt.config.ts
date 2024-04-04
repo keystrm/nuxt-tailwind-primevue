2de3daa2b7264ba3955831d4eaf068fd0e24a98d
Kasun Madushan <kasunsudarshana1@gmail.com>
2024-04-04 06:31:27 +0530

    add tailwind config settings to work

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/color-mode"]
+  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/color-mode"],
+  tailwindcss: {
+    config: {
+      content: ["presets/**/*.{js,vue,ts}"],
+      darkMode: "class",
+    },
+  },
 })
